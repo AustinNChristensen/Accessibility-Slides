d68d1e42195ad5333a55c1059e1ea6afefbb466d
Austin Christensen <austin.christensen37@gmail.com>
2019-10-01 21:07:33 -0500

    update outline, add content

--- src/presentation.js
+++ src/presentation.js
@@ -1,3 +1,4 @@
+/* eslint-disable import/no-webpack-loader-syntax */
 // Import React
 import React from 'react';
 
@@ -13,8 +14,15 @@ import {
   Slide,
   Text,
   Appear,
+  Notes,
   // CodePane,
   ComponentPlayground,
+  Table,
+  TableHeader,
+  TableHeaderItem,
+  TableBody,
+  TableRow,
+  TableItem,
 } from 'spectacle';
 
 // Import theme
@@ -48,9 +56,9 @@ export const Presentation = () => {
         showFullscreenControl={false}
       >
       {/* Title */}
-        <Slide bgColor="primary">
+        <Slide bgColor="primary" transition={['none']}>
           <Heading size={1} fit caps lineHeight={1} textColor="secondary">
-            Web Accessibility 101
+            Web A11y 101
           </Heading>
           <Text margin="10px 0 0" textColor="tertiary" size={1} fit bold>
             Aria && WAI && WCAG 2.0h my
@@ -58,9 +66,19 @@ export const Presentation = () => {
         </Slide>
         <Slide bgColor='secondary'>
           <Heading size={1} textColor='tertiary' fit caps>
-            WTF
+            WTF*
           </Heading>
+          <Text textColor='primary' textSize='16px'>
+            *What the Frontend
+          </Text>
         </Slide>
+        {/* Motivation */}
+        <Slide bgColor='tertiary'>
+          <BlockQuote>
+            <Quote>For many people, technology makes things easier. For people with disabilities, technology makes things possible. </Quote>
+            <Cite textColor='secondary'>Mozilla Developer Network</Cite>
+          </BlockQuote>
+        </Slide> 
         {/* Definitions */}
         <Slide bgColor='tertiary'>
           <Appear>
@@ -102,18 +120,126 @@ export const Presentation = () => {
           </Appear>
           </List>
         </Slide>
-
+        {/* Aria Landmarks */}
+        <Slide>
+          <Heading>Landmarks & Signposts</Heading>
+          <Text>AT can easily navigate using only Landmarks when used correctly</Text>
+          <Text>Content outside of landmarks is referred to as being orphaned</Text>
+        </Slide>
+        {/* Aria Role Equivalents */}
+        <Slide>
+        <Table>
+            <TableHeader>
+              <TableHeaderItem>Aria Role</TableHeaderItem>
+              <TableHeaderItem>HTML 5 Equivalent</TableHeaderItem>
+            </TableHeader>
+            <TableBody>
+              <TableRow>
+                <TableItem>banner</TableItem>
+                <TableItem>none</TableItem>
+              </TableRow>
+              <TableRow>
+                <TableItem>complementary</TableItem>
+                <TableItem>aside</TableItem>
+              </TableRow>
+              <TableRow>
+                <TableItem>contentInfo</TableItem>
+                <TableItem>none</TableItem>
+              </TableRow>
+              <TableRow>
+                <TableItem>form</TableItem>
+                <TableItem>form / div</TableItem>
+              </TableRow>
+              <TableRow>
+                <TableItem>main</TableItem>
+                <TableItem>main</TableItem>
+              </TableRow>
+              <TableRow>
+                <TableItem>navigation</TableItem>
+                <TableItem>nav</TableItem>
+              </TableRow>
+              <TableRow>
+                <TableItem>search</TableItem>
+                <TableItem>none</TableItem>
+              </TableRow>
+            </TableBody>
+          </Table>
+        </Slide>
+        {/* Aria Support */}
+        <Slide>
+        <Heading size={6}>Do browsers support Aria???</Heading>
+          Global support is roughly 95% according to <a style={{ fontStyle: 'none', color: 'inherit' }} href='https://caniuse.com/#feat=wai-aria'>caniuse.com</a>
+        </Slide>
         {/* WAI */}
         <Slide bgColor='primary'>
           <Heading size={1} textColor='secondary'>
             WAI
           </Heading>
+          <List textColor='tertiary'>    
+          <Appear>
+            <ListItem></ListItem>
+          </Appear>
+          <Appear>
+            <ListItem></ListItem>
+          </Appear>
+          <Appear>
+            <ListItem> </ListItem>
+          </Appear>
+          </List>
         </Slide>
         {/* WCAG 2.0 */}
         <Slide bgColor='tertiary'>
           <Heading size={1} textColor='primary'>
             WCAG 2.0
           </Heading>
+          2.1 was standardized in Europe in September 2018
+        </Slide>
+        {/* Perceivable */}
+        <Slide>
+          <Heading>Perceivable</Heading>
+          Is the information presented in a format that the end-user can understand?
+          <Notes>
+            Users must be able to perceive it in some way, using one or more of their senses.
+          </Notes>
+        </Slide>
+        {/* Operable */}
+        <Slide>
+          <Heading>Operable</Heading>
+          <Text>
+            Can the user operate the interface?
+          </Text>
+          <Notes>
+            Buttons
+            Links
+            Modals
+            etc.
+          </Notes>
+        </Slide>
+        {/* Understandable */}
+        <Slide>
+          <Heading>Understandable</Heading>
+          <Text>
+            Is the context of information and operations understandable?
+          </Text>
+        </Slide>
+        {/* Robust */}
+        <Slide>
+          <Heading>Robust</Heading>
+          <Text>
+            Are you using future-proof methods to ensure accessibility into the future?
+          </Text>
+        </Slide>
+        {/* Techniques for Accessibility */}
+        <Slide>
+          <List>
+            <ListItem>Accessible-by-default components</ListItem>
+          </List>
+        </Slide>
+        {/* EXAMPLES */}
+        <Slide>
+          <Heading>Forms</Heading>
+          <a href='https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/forms/Basic_form_hints'>basic forms</a>
+          <a href='https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/forms/alerts'>Adding Alerts</a>
         </Slide>
         {/* Playground Example Attempt */}
         <Slide bgColor='tertiary'>
@@ -154,13 +280,6 @@ export const Presentation = () => {
             <ListItem>Item 3</ListItem>
             <ListItem>Item 4</ListItem>
           </List>
-        </Slide>
-        {/* Quote Example */}
-        <Slide bgColor="secondary" textColor="primary">
-          <BlockQuote>
-            <Quote>Example Quote</Quote>
-            <Cite>Author</Cite>
-          </BlockQuote>
         </Slide>
       </Deck>
     );
